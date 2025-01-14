d13955ebdcf4b84d871b336e6392164fb6c8f455
zmantasha <mantasha@wordscloud.in>
2025-01-14 16:24:07 +0530

    Update page.tsx

--- app/user/upgrade/page.tsx
+++ app/user/upgrade/page.tsx
@@ -1,4 +1,4 @@
-import { Badge } from "@/components/ui/badge";
+import { Badge } from "../../../components/ui/badge";
 import { Check } from "lucide-react";
 import Link from "next/link";
 import styles from "./Upgrade.module.css"; // Assuming CSS module for styling
